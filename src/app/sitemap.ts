29a5f032f177d423ebe62e8e4f561985ca3a23d2
319Webdesign <schmidtmaik1909@web.de>
2026-03-07 22:37:34 +0100

    seo

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,7 +9,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return routes.map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date(),
-    changeFrequency: (route === "" ? "weekly" : "monthly") as const,
+    changeFrequency: route === "" ? "weekly" : "monthly",
     priority: route === "" ? 1 : 0.8,
   }));
 }
